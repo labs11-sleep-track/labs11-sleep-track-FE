94b38cafc0cc682abbcf6c1b0931c3a770e4eb9a
Olivia Osborn <olioz95@gmail.com>
2019-03-21 13:45:34 -0400

    set up router in index.js file and routes to each view in app.js file

--- sleepsta/src/App.js
+++ sleepsta/src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import "./App.css";
+
+import DashboardView from "./views/DashboardView";
+import BlogsView from "./views/BlogsView";
+import LoginView from "./views/LoginView";
+import RegisterView from "./views/RegisterView";
+import ProfileView from "./views/ProfileView";
 
 class App extends Component {
   render() {
@@ -11,6 +18,11 @@ class App extends Component {
           sleep. It also has a web application that allows the user to visualize
           their sleep habits online.
         </p>
+        <Route exact path="/" component={LoginView} />
+        <Route exact path="/register" component={RegisterView} />
+        <Route exact path="/blogs" component={BlogsView} />
+        <Route exact path="/home" component={DashboardView} />
+        <Route exact path="/profile" component={ProfileView} />
       </div>
     );
   }
