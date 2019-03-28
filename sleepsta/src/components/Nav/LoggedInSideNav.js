1b8424652cbc7cf89ddedd890750a5e05fd9de7c
KROHNICK <business.kanekperez@gmail.com>
2019-03-28 15:37:53 -0700

    side nav fixes

--- sleepsta/src/components/Nav/LoggedInSideNav.js
+++ sleepsta/src/components/Nav/LoggedInSideNav.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 import {
   Collapse,
   Navbar,
@@ -11,12 +11,13 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem } from 'reactstrap';
+  DropdownItem
+} from "reactstrap";
 
-import { Link } from 'react-router-dom';
-import { Route, withRouter} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
 
-import './SideBarNav.css';
+import "./SideBarNav.css";
 
 // import LoginForm from '../Login/LoginForm';
 // import Register from '../Register/RegisterForm';
@@ -42,108 +43,143 @@ class LoggedInSideNav extends React.Component {
 
   render() {
     return (
-
-
-<div class="wrapper">
-
-<div id="content">
-    <nav id="menuButton" class="navbar navbar-expand-lg navbar-light ">
-
-        <div class="container-fluid">
-
-
-            <button type="button" id="sidebarCollapse" class="btn btn-info">
-                <i className="fas fa-align-left"></i>
+      <div className="wrapper">
+        <div id="content">
+          <nav
+            id="menuButton"
+            className="navbar navbar-expand-lg navbar-light "
+          >
+            <div className="container-fluid">
+              <button
+                type="button"
+                id="sidebarCollapse"
+                className="btn btn-info"
+              >
+                <i className="fas fa-align-left" />
                 <span>&nbsp;Menu</span>
-            </button>
-
-        </div>
-        
-    </nav>
-</div>
-
-
-    <nav id="sidebar">
-        <div class="sidebar-header">
-        <NavLink href='/'>
-        <div className="sleepTitle">
-        SLEEP<i>STA</i>
-        </div>
-        </NavLink>
-        </div>
-
-        <ul class="list-unstyled components">
-        
-        <div className="catchPhrase">
-            <p>Sleep Harder. Live Smarter.</p>
+              </button>
             </div>
-            
-            <li class="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Why Sleep?</a>
-                <ul class="collapse list-unstyled" id="homeSubmenu">
-                    <li>
-                    <NavItem>
-                        <NavLink href="/whysleep">
-                        <div><i className="fas fa-plus"></i>&nbsp;&nbsp;Learn More</div>
-                        </NavLink>
-                    </NavItem>
-                    </li>
-                </ul>
+          </nav>
+        </div>
+
+        <nav id="sidebar">
+          <div className="sidebar-header">
+            <NavLink href="/">
+              <div className="sleepTitle">
+                SLEEP<i>STA</i>
+              </div>
+            </NavLink>
+          </div>
+
+          <ul className="list-unstyled components">
+            <div className="catchPhrase">
+              <p>Sleep Harder. Live Smarter.</p>
+            </div>
+
+            <li className="active">
+              <a
+                href="#homeSubmenu"
+                data-toggle="collapse"
+                aria-expanded="false"
+                className="dropdown-toggle"
+              >
+                Why Sleep?
+              </a>
+              <ul className="collapse list-unstyled" id="homeSubmenu">
+                <li>
+                  <NavItem>
+                    <NavLink href="/whysleep">
+                      <div>
+                        <i className="fas fa-plus" />
+                        &nbsp;&nbsp;Learn More
+                      </div>
+                    </NavLink>
+                  </NavItem>
+                </li>
+              </ul>
             </li>
 
-            <li class="active">
-                <a href="#pageSubmenu3" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Blogs</a>
-                <ul class="collapse list-unstyled" id="pageSubmenu3">
-                    <li>
-                    <Link style={{textDecoration: "none"}} to="/blogs">
-                        <div><i className="fas fa-rss"></i>&nbsp;&nbsp;Sleep Blogs</div>
-                    </Link>
-                    </li>
-                </ul>
+            <li className="active">
+              <a
+                href="#pageSubmenu3"
+                data-toggle="collapse"
+                aria-expanded="false"
+                className="dropdown-toggle"
+              >
+                Blogs
+              </a>
+              <ul className="collapse list-unstyled" id="pageSubmenu3">
+                <li>
+                  <Link style={{ textDecoration: "none" }} to="/blogs">
+                    <div>
+                      <i className="fas fa-rss" />
+                      &nbsp;&nbsp;Sleep Blogs
+                    </div>
+                  </Link>
+                </li>
+              </ul>
             </li>
 
             <li>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Account</a>
-                <ul class="collapse list-unstyled" id="pageSubmenu">
+              <a
+                href="#pageSubmenu"
+                data-toggle="collapse"
+                aria-expanded="false"
+                className="dropdown-toggle"
+              >
+                Account
+              </a>
+              <ul className="collapse list-unstyled" id="pageSubmenu">
+                <li>
+                  <Link style={{ textDecoration: "none" }} to="/profile">
+                    <div>
+                      <i className="fas fa-chart-bar" />
+                      &nbsp;&nbsp;Profile
+                    </div>
+                  </Link>
+                </li>
 
-                    <li>
-                        <Link style={{textDecoration: "none"}} to="/profile">
-                            <div><i className="fas fa-chart-bar"></i>&nbsp;&nbsp;Profile</div>
-                        </Link>
-                    </li>
-
-                    <li>
-                        <Link style={{textDecoration: "none"}} to="/update">
-                            <div><i className="fas fa-user-edit"></i>&nbsp;&nbsp;Update Account</div>
-                        </Link>
-                    </li>
-
-                </ul>
+                <li>
+                  <Link style={{ textDecoration: "none" }} to="/update">
+                    <div>
+                      <i className="fas fa-user-edit" />
+                      &nbsp;&nbsp;Update Account
+                    </div>
+                  </Link>
+                </li>
+              </ul>
             </li>
 
             <li>
-                <a href="#pageTwoSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Contact</a>
-                <ul class="collapse list-unstyled" id="pageTwoSubmenu">
-
-                    <li>
-                        <Link style={{textDecoration: "none"}} to="/about">
-                            <div><i className="fas fa-bed"></i>&nbsp;&nbsp;SLEEP<i>STA</i> Team</div>
-                        </Link>
-                    </li>
-                </ul>
+              <a
+                href="#pageTwoSubmenu"
+                data-toggle="collapse"
+                aria-expanded="false"
+                className="dropdown-toggle"
+              >
+                Contact
+              </a>
+              <ul className="collapse list-unstyled" id="pageTwoSubmenu">
+                <li>
+                  <Link style={{ textDecoration: "none" }} to="/about">
+                    <div>
+                      <i className="fas fa-bed" />
+                      &nbsp;&nbsp;SLEEP<i>STA</i> Team
+                    </div>
+                  </Link>
+                </li>
+              </ul>
             </li>
-        
+
             <li>
-                  <div>
-                  <a onClick={this.logout}>Logout</a>
-                  </div>
+              <div>
+                <a onClick={this.logout}>Logout</a>
+              </div>
             </li>
-        </ul>
-    </nav>
+          </ul>
+        </nav>
 
-    
-
-    {/* <main>
+        {/* <main>
           <Route path="/login" component={LoginForm} />
           <Route path="/register" component={Register} />  
           <Route path="/update" component={UserForm} />  
@@ -151,14 +187,13 @@ class LoggedInSideNav extends React.Component {
           <Route path="/blogs" component={Blogs} />
           <Route path="/about" component={About} />   
      </main>  */}
+      </div>
+    );
+  }
 
-</div>
-
-)}
-
-logout = () => {
-    localStorage.removeItem('jwt');
-    this.props.history.push('/');
+  logout = () => {
+    localStorage.removeItem("jwt");
+    this.props.history.push("/");
   };
 }
 
