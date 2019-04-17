38edba00ef5edb90c0974cd80fd2c6e9ac804a68
KROHNICK <business.kanekperez@gmail.com>
2019-04-17 09:47:42 -0700

    corrected merge conflicts

--- sleepsta/src/components/Home/Home.js
+++ sleepsta/src/components/Home/Home.js
@@ -1,13 +1,7 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from "react";
 import styled from "styled-components";
 import LoginForm from "../Login/LoginForm";
-import {
-  Link
-} from "react-router-dom";
 import "./Home.css";
-
 import stats from "./img/stats.PNG";
 import bg from "./img/bg.jpg";
 import datasvg from "./img/data.svg";
@@ -18,8 +12,8 @@ import { Link } from "react-router-dom";
 
 //CSS Reset is imported above for homepage. All homepage styling handled within their respective components.
 
-const Main = styled.div `
-  font-family: "Poppins","Roboto";
+const Main = styled.div`
+  font-family: "Poppins", "Roboto";
   /* background-image: linear-gradient(to top, #0a1429 0%, #0f1e3f 100%); */
   position: sticky;
   position: absolute;
@@ -28,7 +22,7 @@ const Main = styled.div `
   height: 100%;
 `;
 
-const Top = styled.div `
+const Top = styled.div`
  display: flex;
  flex-direction: column;
  background-image: url("${bg}");
@@ -42,23 +36,23 @@ const Top = styled.div `
   }
 `;
 
-const LandingContent = styled.div `
+const LandingContent = styled.div`
   display: flex;
   justify-content: space-evenly;
   height: 100%;
   width: 100%;
 
-  @media(max-width: 800px){
+  @media (max-width: 800px) {
     margin-top: 36px;
     flex-direction: column;
   }
 
-  @media(max-height: 500px){
+  @media (max-height: 500px) {
     flex-direction: column;
   }
 `;
 
-const AttentionHold = styled.div `
+const AttentionHold = styled.div`
  width: 50%;
  padding: 20px 20px 0px 20px;
 
@@ -68,7 +62,7 @@ const AttentionHold = styled.div `
  }
 `;
 
-const AttentionGrab = styled.div `
+const AttentionGrab = styled.div`
   background-image: url("${stats}");
   background-repeat:no-repeat;
   background-size:cover;
@@ -76,39 +70,39 @@ const AttentionGrab = styled.div `
   width: 100%;
 `;
 
-const HeaderHold = styled.div `
+const HeaderHold = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 
-  @media(max-width: 800px){
+  @media (max-width: 800px) {
     padding-top: 20px;
     width: 100%;
- }
+  }
 
-  @media(max-height: 500px){
+  @media (max-height: 500px) {
     flex-direction: row;
   }
 
-  h2{
+  h2 {
     text-align: right;
     padding: 3px;
   }
 `;
 
-const HeadBG = styled.div `
+const HeadBG = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: rgb(0,0,0,.65);
-  border: 1px solid rgb(255,255,255,.15);
+  background-color: rgb(0, 0, 0, 0.65);
+  border: 1px solid rgb(255, 255, 255, 0.15);
   padding: 20px;
   margin: 10px;
   border-radius: 15px;
-`
+`;
 
-const HeaderH1 = styled.h1 ` 
+const HeaderH1 = styled.h1`
   font-family: "Poppins", "Roboto";
   font-size: 6em;
   z-index: 1;
@@ -117,36 +111,35 @@ const HeaderH1 = styled.h1 `
     padding-top: 10px;
     font-size: 4.5em;
   }
+`;
 
-  `;
-
-const TaglineBar = styled.div `
+const TaglineBar = styled.div`
   width: 100%;
-  background-color: rgb(255,255,255,.05);
+  background-color: rgb(255, 255, 255, 0.05);
   border-top: 1px solid #e34a6f;
   z-index: 3;
 `;
 
-const WelcomeHold = styled.div `
+const WelcomeHold = styled.div`
   margin-top: 20px;
   width: 100%;
   display: flex;
   justify-content: center;
 `;
 
-const WelcomeTo = styled.div `
+const WelcomeTo = styled.div`
   width: 92%;
-  background-color: rgb(255,255,255,.95);
+  background-color: rgb(255, 255, 255, 0.95);
   border-radius: 15px;
   display: flex;
   justify-content: center;
   height: 500px;
-  color: rgb(25,25,25);
+  color: rgb(25, 25, 25);
   overflow: hidden;
   padding: 30px;
   box-shadow: 3px 3px 0px 1.5px black;
 
-  @media(max-width: 800px){
+  @media (max-width: 800px) {
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
@@ -154,26 +147,26 @@ const WelcomeTo = styled.div `
   }
 `;
 
-const WelcomeLeft = styled.div `
+const WelcomeLeft = styled.div`
   width: 60%;
   display: flex;
   justify-content: center;
   align-items: center;
-img {
+  img {
     width: 90%;
     height: 90%;
-  } 
-  @media(max-width: 800px){
+  }
+  @media (max-width: 800px) {
     width: 100%;
 
-    img{ 
+    img {
       width: 80%;
       height: 80%;
     }
   }
 `;
 
-const WelcomeRight = styled.div `
+const WelcomeRight = styled.div`
   width: 60%;
   display: flex;
   justify-content: center;
@@ -181,14 +174,14 @@ const WelcomeRight = styled.div `
   text-align: center;
   font-size: 2.4em;
 
-  @media(max-width: 800px){
+  @media (max-width: 800px) {
     width: 100%;
     text-align: center;
     padding: 30px;
   }
 `;
 
-const FeatureBlocks = styled.div `
+const FeatureBlocks = styled.div`
   margin-top: 20px;
   width: 100%;
   display: flex;
@@ -196,34 +189,34 @@ const FeatureBlocks = styled.div `
   justify-content: space-evenly;
   align-items: center;
 
-  @media(max-width: 800px){
+  @media (max-width: 800px) {
     flex-direction: column;
   }
 `;
 
-const FBHold = styled.div `
+const FBHold = styled.div`
   width: 30%;
   height: 800px;
   margin: 10px;
   border-radius: 15px;
-  background-color: rgb(255,255,255,.05);
+  background-color: rgb(255, 255, 255, 0.05);
 
-  @media(max-width: 800px){
+  @media (max-width: 800px) {
     width: 90%;
   }
 `;
 
-const SVGHold = styled.div `
+const SVGHold = styled.div`
   width: 100%;
   height: 50%;
   display: flex;
   justify-content: center;
-  
+
   img {
     width: 85%;
     padding: 30px;
   }
-`
+`;
 
 const FooterAll = styled.div`
   margin: 0 auto;
@@ -280,25 +273,20 @@ const TextSleep = styled.p`
   @media (max-width: 500px) {
     font-size: 10px;
   }
+`;
 
-const FBTextHold = styled.div `
+const FBTextHold = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   height: 50%;
 `;
 
-const FBTextHead = styled.h3 `
+const FBTextHead = styled.h3``;
 
-`;
-
-const FBTextContent = styled.p `
-
-
-`;
+const FBTextContent = styled.p``;
 
 class Home extends Component {
-
   render() {
     return (
       <Main>
@@ -357,9 +345,8 @@ class Home extends Component {
           </TextSleep>
         </FooterAll>
       </Main>
-
     );
-  };
+  }
 }
 
 export default Home;
